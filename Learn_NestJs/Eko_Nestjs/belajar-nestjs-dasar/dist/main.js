ceff42a27253e2c5cdf08b1506ec31134e2bcb0e
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-05-20 15:10:31 +0700

    2024-05-20 (config env port)

--- Learn_NestJs/Eko_Nestjs/belajar-nestjs-dasar/dist/main.js
+++ Learn_NestJs/Eko_Nestjs/belajar-nestjs-dasar/dist/main.js
@@ -4,13 +4,15 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const cookieParser = require("cookie-parser");
 const mustache = require("mustache-express");
+const config_1 = require("@nestjs/config");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use(cookieParser('MY SECRET KEY'));
     app.set('views', __dirname + '/../views');
     app.set('view engine', 'html');
     app.engine('html', mustache());
-    await app.listen(3003);
+    const configService = app.get(config_1.ConfigService);
+    await app.listen(configService.get('PORT'));
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- Learn_NestJs/Eko_Nestjs/belajar-nestjs-dasar/src/main.ts
+++ Learn_NestJs/Eko_Nestjs/belajar-nestjs-dasar/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as mustache from 'mustache-express';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,7 +13,8 @@ async function bootstrap() {
   app.set('view engine', 'html');
   app.engine('html', mustache());
 
-  await app.listen(3003);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();
