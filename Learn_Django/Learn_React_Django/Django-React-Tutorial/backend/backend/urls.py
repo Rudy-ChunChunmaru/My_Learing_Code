e6387b757fb8d0fd28b32b7a48c3a26403af8736
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-07-22 13:04:01 +0700

    add Lean code Django React Tutorial

---
+++ Learn_Django/Learn_React_Django/Django-React-Tutorial/backend/backend/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from api.views import CrateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/user/register/",CrateUserView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refr")
+
+]
