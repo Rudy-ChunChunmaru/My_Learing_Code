4ed7d75829ef6e8d68d6678ef819fcbcea38ea19
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-12-05 21:46:47 +0700

    update payment gateway

--- Learn_React/react-payment-gateway/src/App.tsx
+++ Learn_React/react-payment-gateway/src/App.tsx
@@ -3,20 +3,32 @@ import Bottombar from "./components/bottombar";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [numberOrder] = useState(Math.random()*100)
-  const [price,setPrice] = useState(0)
-  const [pay,setPay] = useState(false)
+  const [numberOrder] = useState(Math.random() * 100);
+  const [price, setPrice] = useState(0);
+  const [pay, setPay] = useState(false);
 
-  useEffect(()=>{
-    if(pay){
-      alert('payyy !!!!')
-      setPay(false)
+  let paramenter = {
+    item_details: {
+      name: "book",
+      price: price,
+    },
+    transaction_details: {
+      order_id: numberOrder,
+      gross_amount: price,
+    },
+  };
+
+  useEffect(() => {
+    if (pay) {
+      alert("pay");
+      setPay(false);
     }
-  },[pay])
+  }, [pay]);
 
   return (
-    <div className="bg-orange-100 h-screen w-screen relative overflow-x-auto
-    text-xs py-14"
+    <div
+      className="relative h-screen w-screen overflow-x-auto bg-orange-100
+    py-14 text-xs"
     >
       <div className="w-full px-10">
         <Item setPrice={setPrice} />
