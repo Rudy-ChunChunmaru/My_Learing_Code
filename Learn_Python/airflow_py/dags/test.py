9879cdc06bb99b877a3d0b633d8ce76b2a66684e
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2025-09-07 15:09:43 +0700

    airflow and drizzle

---
+++ Learn_Python/airflow_py/dags/test.py
@@ -0,0 +1,3 @@
+from airfrow import DAG
+from airflow.operators.dummy import DummyOperator  
+
---
+++ Learn_Python/airflow_py/postgres/superset-init.sql
@@ -0,0 +1,4 @@
+CREATE USER superset WITH PASSWORD 'superset';
+CREATE DATABASE superset_db OWNER superset;
+CREATE USER examples WITH PASSWORD 'examples';
+CREATE DATABASE examples_db OWNER examples
